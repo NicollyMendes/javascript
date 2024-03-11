7cc7fbbddd396770b78be67c866d40358fe0468c
NicollyMendes <146892975+NicollyMendes@users.noreply.github.com>
2024-03-11 12:14:25 -0400

    atualizei aulas

---
+++ aula13/ambiente.js
@@ -0,0 +1,34 @@
+for (var c=1; c<=10; c++){
+
+}
+
+
+
+/*var c = 1
+
+ do{
+    console.log(`Passo ${c}`)
+    c++ 
+ } while (c <= 57)
+*/
+
+
+
+
+/*
+var c =  1
+while( c <= 52 ){
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+}
+*/
+
+
+/*
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?') 
+*/
